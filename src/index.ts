a1e63e0db2a8df32803349ad4b678b3647440128
inovue <info@sinoue.net>
2023-12-03 01:23:11 +0900

    fix: mian.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
-import {Transformer, unified} from 'unified';
+import type { Root } from "mdast";
+import {unified} from 'unified';
 //import remark from 'remark'
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
@@ -11,14 +12,7 @@ import remarkGfm from "remark-gfm";
 import remarkComment from "remark-comment";
 import type { Options } from 'remark-comment';
 
-import type { Node, Parent } from 'unist';
-import { visit } from 'unist-util-visit';
-
-// プラグインの型定義
-interface CommentNode extends Node {
-  type: 'comment';
-  commentValue: string;
-}
+import { remarkCommentExpand } from "./plugins/remarkCommentExpand.js";
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -26,30 +20,15 @@ const __dirname = path.dirname(__filename);
 
 (async () => {
 
-  const remarkCommentExpand: () => Transformer = () => {
-    return (tree) => {
-      visit(tree, 'comment', (node:CommentNode, index, parent:Parent) => {
-        if (parent && typeof index === 'number') {
-          // commentValueのマークダウンをmdastに変換
-          const mdastTree = unified().use(remarkParse).use(remarkGfm).parse(node.commentValue);
-  
-          // 元のcommentノードを変換したノードに置き換え
-          parent.children.splice(index, 1, ...mdastTree.children);
-        }
-      });
-    };
-  };
-
   const mdString = fs.readFileSync(path.resolve(__dirname, "example.md")).toString();
-  // console.log(mdString);
 
   const mdast = unified()
     .use(remarkParse)
     .use(remarkGfm)
     .use(remarkComment, { ast: true } as Options)
     .parse(mdString);
-  const mdast2 = unified().use(remarkCommentExpand).runSync(mdast);
-  const hast = unified().use(remarkRehype).runSync(mdast);
+  const mdast2 = unified().use(remarkCommentExpand).runSync(mdast) as Root;
+  const hast = unified().use(remarkRehype).runSync(mdast2);
   const htmlString = unified().use(rehypeStringify).stringify(hast);
 
   /*
